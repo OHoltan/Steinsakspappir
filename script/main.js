dc60eca6faacc78fbcb1bb427ff6f639e4fdcd65
OHoltan <ola.holtan2303@gmail.com>
2023-08-31 10:25:33 +0200

    Itteration 2
    added page 2 css html and defined all DOM elements in JS

---
+++ script/main.js
@@ -0,0 +1,32 @@
+// player 1 images
+const steinP1 = document.querySelector(".stein1")
+const saksP1 = document.querySelector(".saks1")
+const papirP1 = document.querySelector(".papir1")
+
+
+// player 2 images
+const steinP2 = document.querySelector(".stein2")
+const saksP2 = document.querySelector(".saks2")
+const papirP2 = document.querySelector(".papir2")
+
+// score board
+const scoreBoard = document.querySelector("#scoreboard")
+
+// countdown
+const countdown = document.querySelector("#countdown")
+
+// start button
+const startButton = document.querySelector("#startButton")
+
+// user selection
+const steinButton = document.querySelector("#steinButton")
+const saksButton = document.querySelector("#saksButton")
+const papirButton = document.querySelector("#pappirButton")
+
+// home button
+const homeButton = document.querySelector("#home")
+
+
+// active images for players
+let activeImgP1 = steinP1
+let activeImgP2 = steinP2
