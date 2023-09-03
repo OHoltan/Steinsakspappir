9bfe3f5b070593a780e1f43de710da0af4206241
OHoltan <96868594+OHoltan@users.noreply.github.com>
2023-09-03 12:40:41 +0200

    First Logic pass
    Added first logic, blinking, start, and countdown

--- script/main.js
+++ script/main.js
@@ -13,7 +13,7 @@ const papirP2 = document.querySelector(".papir2")
 const scoreBoard = document.querySelector("#scoreboard")
 
 // countdown
-const countdown = document.querySelector("#countdown")
+const countdownelement = document.querySelector("#countdown")
 
 // start button
 const startButton = document.querySelector("#startButton")
@@ -26,7 +26,84 @@ const papirButton = document.querySelector("#pappirButton")
 // home button
 const homeButton = document.querySelector("#home")
 
+// elements for start/stop game
+const startGameElements = document.querySelectorAll(".start")
 
 // active images for players
 let activeImgP1 = steinP1
 let activeImgP2 = steinP2
+
+
+
+let time = 2
+
+
+startButton.addEventListener('click', function() {
+  gamestarter()
+});
+
+
+
+function gamestarter() {
+    startGameElements.forEach((Element) => Element.classList.toggle("hidden"))
+
+
+
+    countdown()
+}
+
+
+function countdown() {
+    
+    const countdownInterval = setInterval(() => {
+        if (time === 0) {
+          countdownelement.textContent = 'vis';
+          clearInterval(randomImg);
+          clearInterval(countdownInterval);
+        } else {
+          countdownelement.textContent = time;
+          time--;
+        }
+    
+    }, 1000);
+
+    const randomImg = setInterval(() => {
+        const p1RandomNum = Math.floor(Math.random() * 3);
+        const p2RandomNum = Math.floor(Math.random() * 3);
+
+        console.log(p1RandomNum, p2RandomNum)
+
+        if (p1RandomNum == 0) {
+            activeImgP1.classList.toggle("hidden")
+            steinP1.classList.toggle("hidden")
+            activeImgP1 = steinP1
+        } else if (p1RandomNum == 1) {
+            activeImgP1.classList.toggle("hidden")
+            saksP1.classList.toggle("hidden")
+            activeImgP1 = saksP1
+        } else if(p1RandomNum == 2) {
+            activeImgP1.classList.toggle("hidden")
+            papirP1.classList.toggle("hidden")
+            activeImgP1 = papirP1
+        }
+
+        if (p2RandomNum == 0) {
+            activeImgP2.classList.toggle("hidden")
+            steinP2.classList.toggle("hidden")
+            activeImgP2 = steinP2
+        } else if (p2RandomNum == 1) {
+            activeImgP2.classList.toggle("hidden")
+            saksP2.classList.toggle("hidden")
+            activeImgP2 = saksP2
+        } else if(p2RandomNum == 2) {
+            activeImgP2.classList.toggle("hidden")
+            papirP2.classList.toggle("hidden")
+            activeImgP2 = papirP2
+        }
+
+    }, 100);
+
+}
+
+
+
